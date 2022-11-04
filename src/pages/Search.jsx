3fa1f47f0c9da641bfc17e87d887572f6038377c
aditya <adityakadali00@gmail.com>
2022-11-05 01:28:48 +0530

    added search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 function Search() {
   const URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:";
   const [items, setItems] = useState([]);
@@ -11,18 +10,29 @@ function Search() {
       .then((object) => {
         setItems(object.items);
       });
-  });
+  }, []);
 
   return (
-    <ul>
+    <div className="py-10">
+      <h1 className="text-xl font-bold">Showing results for {key}</h1>
       {items.map((e, i) => {
         return (
-          <li key={i}>
-            {e?.volumeInfo?.title} - {e?.volumeInfo?.averageRating}
-          </li>
+          <div key={i} className="mt-7 flex flex-col gap-6">
+            <div className="flex">
+              <img
+                className="w-48"
+                src={e?.volumeInfo?.imageLinks?.smallThumbnail}
+                alt={e?.volumeInfo?.title}
+              />
+              <div className="px-7 py-4">
+                <h1 className="text-xl font-bold">{e?.volumeInfo?.title}</h1>
+                <p className="mt-2">{e?.volumeInfo?.authors}</p>
+              </div>
+            </div>
+          </div>
         );
-      })}
-    </ul>
+      })}{" "}
+    </div>
   );
 }
 
