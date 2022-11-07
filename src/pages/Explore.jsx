e352b23554ba2a7a0e00ef1fe8b24648541a9d84
aditya <adityakadali00@gmail.com>
2022-11-07 23:55:11 +0530

    Fixed carousel

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -8,13 +8,41 @@ function Explore() {
   const [data, setData] = useState([]);
   var settings = {
     dots: true,
+    autoplay: true,
+    pauseOnHover: true,
     infinite: true,
+    speed: 400,
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
   useEffect(() => {
     fetch(DataURL)
       .then((res) => res.json())
