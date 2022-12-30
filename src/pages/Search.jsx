582df247ace6e32a20a729656172f68eed5864f0
AdityaKadali <adityakadali00@gmail.com>
2022-12-30 20:06:04 +0530

    Added book card component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Card from "../components/BookCard";
 
 function Search() {
   let { key } = useParams();
@@ -21,33 +22,16 @@ function Search() {
       .then((object) => {
         setItems(object.items);
       });
+    // eslint-disable-next-line
   }, [key]);
 
   return (
     <div className="py-10">
       <h1 className="text-xl font-bold">Showing results for {key}</h1>
       <div className="grid-cols-1 md:grid md:grid-cols-2">
-        {items.map((e, i) => {
-          return (
-            <div key={i} className=" mt-6 flex" to={e.id}>
-              <img
-                className="w-20 object-cover"
-                src={e?.volumeInfo?.imageLinks?.smallThumbnail.replace(
-                  /&edge=curl/gi,
-                  ""
-                )}
-                alt={e?.volumeInfo?.title}
-              />
-              <div className="px-7 py-4">
-                <h2 className="text-lg font-bold">{e?.volumeInfo?.title}</h2>
-                <p className="mt-2">
-                  <span className="font-bold">Author: </span>
-                  {e?.volumeInfo?.authors}
-                </p>
-              </div>
-            </div>
-          );
-        })}
+        {items.map((e, i) => (
+          <Card data={e} key={i} />
+        ))}
       </div>
     </div>
   );
